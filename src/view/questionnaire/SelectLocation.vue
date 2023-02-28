0de09fe4afff2a784f69170e4000b3b49ee8e671
kth <sksskaw@naver.com>
2023-03-01 04:22:31 +0900

    다음 주소 API 연동 완료

--- src/view/questionnaire/SelectLocation.vue
+++ src/view/questionnaire/SelectLocation.vue
@@ -10,15 +10,40 @@
         </div>
 
         <div class="sub-title">상세 주소는 기입하실 필요 없어요!</div>
+
+        <div class="search-box">
+            <span @click="searchAddress">
+                <input class="search-input" type="text" v-model="address" placeholder="시/군/구/동을 입력해주세요." disabled>
+            </span>
+            <input class="search-icon" type="button" @click="searchAddress">
+        </div>
+
+        <div class="current-location-btn">
+            <img class="target-icon" src="/images/target_icon.png">현재 위치로 검색
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            address: null
+        }
+    },
+
     methods: {
         onBackBtn() {
             this.$router.go(-1);
         },
+
+        searchAddress() {
+            new daum.Postcode({
+                oncomplete: (data) => {
+                    this.address = data.address;
+                }
+            }).open();
+        }
     }
 }
 </script>
@@ -61,5 +86,70 @@ export default {
     color: #AEAEAE;
 
     margin-bottom: 26px;
+}
+
+.search-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    height: 51px;
+
+    border-bottom: 1px solid #e5e5e5;
+    margin-bottom: 17px;
+}
+
+.search-input {
+    border: none;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+
+    width: 287px;
+    cursor: pointer;
+}
+
+.search-input::placeholder {
+    color: #AEAEAE;
+}
+
+.search-input:disabled {
+    background: white;
+}
+
+.search-icon {
+    background: url("../../../public/images/search_icon.png");
+    background-size: cover;
+    border: none;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+}
+
+.current-location-btn {
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 18px 20px;
+    gap: 10px;
+
+    height: 50px;
+
+    border: 1px solid #AEAEAE;
+    border-radius: 8px;
+
+    cursor: pointer;
+}
+
+.target-icon {
+    width: 24px;
+    height: 24px;
 }
 </style>
