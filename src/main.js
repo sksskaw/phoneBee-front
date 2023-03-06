599931f763508867972473d3b7e268d09a8b2848
kth <sksskaw@naver.com>
2023-03-06 23:19:10 +0900

    GTM 적용 견적 확정 페이지 구현 중

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import "@/styles/common.css";
 
+import VueGtag from 'vue-gtag'
+
 const app = createApp(App)
-    .use(router);
+    .use(router)
+    .use(VueGtag, {
+        config: { id: "GA_MEASUREMENT_ID" }
+    })
 
 app.mount("#app");
--- src/view/confirm/ConfirmEstimate.vue
+++ src/view/confirm/ConfirmEstimate.vue
@@ -4,64 +4,82 @@
             <img class="back-btn" src="/images/arrow.png" @click="onBackBtn">
         </div>
 
-        <div class="party-popper-box">
-            <img src="/images/party_popper.svg">
-            <div class="party-popper-text">
-                <span style="color: #E5B40F;">{{ today }}</span>에 선택하신<br>
-                견적이 확정되었습니다!
-            </div>
-        </div>
 
-        <div class="estimate-info">
-            <div class="ei-title">
-                <div>확정된 견적 내역</div>
-                <div style="color: #E5B40F;">{{ new Date().getFullYear() }}년 {{ today }}</div>
-            </div>
-            <div class="cautionary">* 방문 일자가 달라지면 견적 금액이 변경될 수 있습니다.</div>
-
-            <div class="ei-model">
-                <div class="sub-title">핸드폰 기기명</div>
-                <div class="info-box">갤럭시 A53 5G 자급제</div>
+        <div class="container">
+            <div class="party-popper-box">
+                <img src="/images/party_popper.svg">
+                <div class="party-popper-text">
+                    <span style="color: #E5B40F;">{{ today }}</span>에 선택하신<br>
+                    견적이 확정되었습니다!
+                </div>
             </div>
 
-            <div class="ei-total-dc">
-                <div class="sub-title">총 할인 금액</div>
-                <div class="amount">189,134원</div>
-            </div>
+            <div class="estimate-info">
+                <div class="ei-title">
+                    <div>확정된 견적 내역</div>
+                    <div style="color: #E5B40F;">{{ new Date().getFullYear() }}년 {{ today }}</div>
+                </div>
+                <div class="cautionary">* 방문 일자가 달라지면 견적 금액이 변경될 수 있습니다.</div>
 
-            <div class="ei-monthly-amount">
-                <div class="sub-title">단말기 월 할부금</div>
-                <div class="amount">19,000원</div>
-            </div>
-
-            <div class="ei-condition">
-                <div class="sub-title">매장 특별 지원금 지급 조건</div>
-                <div class="info-box">99 요금제 6개월 유지</div>
-            </div>
-
-            <div class="ei-store-info">
-                <div class="sub-title">판매점 정보</div>
-                <div class="si-box">
-                    <div class="si-box-name">상호명</div>
-                    <div>싸다폰</div>
+                <div class="ei-model">
+                    <div class="sub-title">핸드폰 기기명</div>
+                    <div class="info-box">갤럭시 A53 5G 자급제</div>
                 </div>
 
-                <div class="si-box">
-                    <div class="si-box-name">주소</div>
-                    <div>{{ storeAddress }}</div>
+                <div class="ei-total-dc">
+                    <div class="sub-title">총 할인 금액</div>
+                    <div class="amount">189,134원</div>
                 </div>
 
-                <div class="si-box">
-                    <div class="si-box-name"></div>
-                    <div>
-                        <img src="/images/copy.svg" @click="copyAddress">
+                <div class="ei-monthly-amount">
+                    <div class="sub-title">단말기 월 할부금</div>
+                    <div class="amount">19,000원</div>
+                </div>
+
+                <div class="ei-condition">
+                    <div class="sub-title">매장 특별 지원금 지급 조건</div>
+                    <div class="info-box">99 요금제 6개월 유지</div>
+                </div>
+
+                <div class="ei-store-info">
+                    <div class="sub-title">판매점 정보</div>
+                    <div class="si-box">
+                        <div class="si-box-name">상호명</div>
+                        <div>싸다폰</div>
+                    </div>
+
+                    <div class="si-box">
+                        <div class="si-box-name">주소</div>
+                        <div>{{ storeAddress }}</div>
+                    </div>
+
+                    <div class="si-box">
+                        <div class="si-box-name"></div>
+                        <div>
+                            <img src="/images/copy.svg" @click="copyAddress">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="ei-map" id="ei-map"></div>
+            </div>
+
+            <div class="phrase">
+                sample 확정된 견적 그대로,<br>
+                상담없이 바로 개통 가능해요.
+            </div>
+
+            <div class="reservation-date">
+                <div style="font-weight: 700; font-size: 16px;">매장 방문 가능 시간 선택</div>
+                <div class="week-box">날짜 선택
+                    <div class="weeks">
+                        <div class="date">
+
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="ei-map"></div>
         </div>
-
     </div>
 </template>
 
@@ -75,23 +93,72 @@ export default {
         }
     },
 
+    mounted() {
+        if (window.kakao && window.kakao.maps) {
+            this.initMap();
+        } else {
+            const script = document.createElement("script");
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src =
+                "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=" + process.env.VUE_APP_KAKAO_URL + "&libraries=services";
+            document.head.appendChild(script);
+        }
+    },
+
     methods: {
         onBackBtn() {
             this.$router.go(-1);
         },
 
         copyAddress() {
-            navigator.clipboard.writeText(this.storeAddress)
-        }
+            const textArea = document.createElement('textarea')
+            document.body.appendChild(textArea)
+            textArea.value = this.storeAddress
+            textArea.select()
+            document.execCommand('copy')
+            document.body.removeChild(textArea)
+        },
+
+        initMap() {
+            const container = document.getElementById("ei-map");
+            const options = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                level: 5,
+            };
+
+            this.map = new kakao.maps.Map(container, options);
+            this.geocoder = new kakao.maps.services.Geocoder();
+            this.marker = new kakao.maps.Marker({
+                position: new kakao.maps.LatLng(33.450701, 126.570667),
+                map: this.map
+            });
+
+            this.geocoder.addressSearch(this.storeAddress, (result, status) => {
+                if (status === kakao.maps.services.Status.OK) {
+
+                    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+                    this.map.setCenter(coords);
+                    this.marker.setPosition(coords)
+                }
+            });
+        },
     }
 }
 </script>
 
 <style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    padding: 0px;
+    gap: 12px;
+
+    background-color: #f0f0f0;
+}
+
 .back-btn-box {
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: flex-start;
     height: 60px;
 }
@@ -110,7 +177,7 @@ export default {
     gap: 8px;
 
     height: 208px;
-    margin-bottom: 12px;
+    background-color: white;
 }
 
 .party-popper-text {
@@ -133,7 +200,6 @@ export default {
     padding: 28px 24px;
     gap: 16px;
 
-    height: 696px;
     background: #FFFFFF;
 }
 
@@ -280,5 +346,62 @@ export default {
     height: 160px;
     border-radius: 4px;
     background-color: gainsboro;
+}
+
+.phrase {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-around;
+    align-items: center;
+    height: 116px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 21.33px;
+    line-height: 30px;
+
+    color: #000000;
+    background: #FFFFFF;
+}
+
+.reservation-date {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 28px 24px;
+    gap: 24px;
+
+    height: 346px;
+    background: #FFFFFF;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 22px;
+
+    color: #000000;
+}
+
+.week-box {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 12px;
+    height: 152px;
+}
+
+.weeks {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 20px 8px;
+    gap: 4px;
+
+    height: 118px;
 }
 </style>
