dd21039091bf3aca90c09258b2098cb893891d56
kth <sksskaw@naver.com>
2023-03-10 12:00:09 +0900

    견적 요청 구현 중

--- src/view/questionnaireCompleted/Loading.vue
+++ src/view/questionnaireCompleted/Loading.vue
@@ -7,51 +7,115 @@
         <img class="loading-img" src="/images/loading.gif">
 
         <div class="loading-page-box-1 animate__animated animate__fadeInUp">
-            {{ address }} 주변 매장들의<br>
+            {{ sigungu }} 주변 매장들의<br>
             {{ estimateText }} 견적을<br>
             확인하고 있어요.
         </div>
 
         <div class="loading-page-box-2 animate__animated animate__fadeInUp">
-            견적을 비교하여 최적의 선택지를<br>
-            알려드릴게요!
+            견적을 비교하여<br>
+            최적의 선택지를 알려드릴게요!
         </div>
     </div>
 </template>
 
 <script>
+import apiQuestionnaire from '@/api/questionnaire';
+import cookie from '@/utils/cookie';
+
 export default {
     data() {
         return {
-            address: localStorage.getItem('address'),
+            sigungu: localStorage.getItem('sigungu'),
             estimateText: null,
         }
     },
 
     created() {
-        var lookingForModelCheck = localStorage.getItem('lookingForModelCheck');
-        var selectBillPaid = localStorage.getItem('selectBillPaid');
-        var selectedModel = localStorage.getItem('selectedModel');
-
-        if (lookingForModelCheck == "0") {
-            this.estimateText = selectBillPaid + " 원대"
-        }
-
-        if (lookingForModelCheck == "1") {
-            this.estimateText = selectedModel + "번 모델"
-        }
+        this.setEstimateBtnText()
     },
 
     mounted() {
-        setTimeout(() => {
-            this.$router.push("/questionnaireCompleted/Found");
-        }, 3000);
+        var enmemberidx = cookie.getCookie('Enmemberidx')
+        if (enmemberidx == '' || enmemberidx == null)
+            this.$router.push("/");
+
+        var lookingForModelCheck = localStorage.getItem('lookingForModelCheck')
+
+        var selectedModel = JSON.parse(localStorage.getItem('selectedModel'))
+        var selectedMobileCarrier = JSON.parse(localStorage.getItem('selectedMobileCarrier'))
+        var selectedUsagePeriod = JSON.parse(localStorage.getItem('selectedUsagePeriod'))
+        var selectBillPaid = JSON.parse(localStorage.getItem('selectBillPaid'))
+
+        var sido = localStorage.getItem('sido')
+        var sigungu = localStorage.getItem('sigungu')
+
+        // 원하는 기기 있는 경우
+        if (lookingForModelCheck == "1") {
+            let param = {
+                useTelecomIdx: parseInt(selectedMobileCarrier.value),
+                usePeriodIdx: parseInt(selectedUsagePeriod.value),
+                deviceIdx: parseInt(selectedModel.value),
+                findArea: encodeURIComponent(sido + " " + sigungu),
+            }
+            console.log(param)
+            this.postSurveyDeviceComplete(param, enmemberidx)
+        }
+
+        // 원하는 기기 없는 경우
+        if (lookingForModelCheck == "0") {
+            let param = {
+                useTelecomIdx: parseInt(selectedMobileCarrier.value),
+                usePeriodIdx: parseInt(selectedUsagePeriod.value),
+                monthCost: parseInt(selectBillPaid.value),
+                findArea: encodeURIComponent(sido + " " + sigungu),
+            }
+            console.log(param)
+            this.postSurveyCostComplete(param, enmemberidx)
+
+        }
     },
 
     methods: {
         onBackBtn() {
             this.$router.push("/questionnaire/selectLocation");
         },
+
+        setEstimateBtnText() {
+            var lookingForModelCheck = localStorage.getItem('lookingForModelCheck')
+            var selectedModel = JSON.parse(localStorage.getItem('selectedModel'))
+            var selectBillPaid = JSON.parse(localStorage.getItem('selectBillPaid'))
+
+            if (lookingForModelCheck == "0") {
+                this.estimateText = selectBillPaid.name
+            }
+
+            if (lookingForModelCheck == "1") {
+                this.estimateText = selectedModel.name
+            }
+        },
+
+        postSurveyDeviceComplete(param, enmemberidx) {
+            apiQuestionnaire.postSurveyDeviceComplete(param, enmemberidx)
+                .then(response => {
+                    console.log(response)
+                    //this.$router.push("/questionnaireCompleted/loading");
+                })
+                .catch(e => {
+                    console.log(e)
+                });
+        },
+
+        postSurveyCostComplete(param, enmemberidx) {
+            apiQuestionnaire.postSurveyCostComplete(param, enmemberidx)
+                .then(response => {
+                    console.log(response)
+                    //this.$router.push("/questionnaireCompleted/loading");
+                })
+                .catch(e => {
+                    console.log(e)
+                });
+        }
     }
 }
 </script>
