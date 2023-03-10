cf1de8554f7d75848784d140ccc91c146e5c1859
kth <sksskaw@naver.com>
2023-03-10 13:08:21 +0900

    오류 수정

--- src/api/estimate.js
+++ src/api/estimate.js
@@ -17,4 +17,21 @@ export default {
                 });
         });
     },
+
+    async getEstimateList(surveyCode) {
+        return new Promise((resolve, reject) => {
+            rest.get(`/api/survey/estimate/list/${surveyCode}`
+                , {
+                    headers: {
+                        'Enmemberidx': 'SXlTSHdxQnZrS2pKTkRUYm5FMU15UT09'
+                    }
+                })
+                .then(response => {
+                    resolve(response);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    },
 }
