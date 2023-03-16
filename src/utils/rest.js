b253301945320763b66adf9fc8d1ed08736eb86b
kth <sksskaw@naver.com>
2023-03-16 19:31:36 +0900

    API 예외처리 및 로그인 페이지 수정

--- src/utils/rest.js
+++ src/utils/rest.js
@@ -4,4 +4,30 @@ const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
 });
 
+instance.interceptors.response.use(
+    function (response) {
+        if (response.data.message.strType == 'alert_and_back') {
+            alert(response.data.message.strMessage)
+            this.$router.go(-1);
+            return
+        }
+        
+        if (response.data.message.strType == 'alert') {
+            alert(response.data.message.strMessage)
+            return
+        }
+
+        if (response.data.message.strType == 'alert_and_go_login') {
+            alert(response.data.message.strMessage)
+            this.$router.push('/login');
+            return
+        }
+
+        return response
+    },
+    function (error) {
+        alert(error)
+    },
+);
+
 export default instance;
--- src/view/KakaoLogin.vue
+++ src/view/KakaoLogin.vue
@@ -6,8 +6,12 @@
             <div style="width: 24px; height: 24px;"></div>
         </div>
 
-        <div class="login-btn">
-            <img src="/images/kakao_login.png">
+        <div class="login-btn" v-if="loading == false" @click="onLogin">
+            <img class="loading-icon" src="/images/kakao_login.png">
+        </div>
+
+        <div class="login-loading-btn" v-if="loading == true">
+            <img style="width: 30px; height: 30px;" class="loading-icon" src="/images/search_loading_icon.gif">
         </div>
     </div>
 
@@ -18,15 +22,69 @@
 
 <script>
 import Footer from '@/components/MainFooter.vue';
+import apiLogin from '@/api/login';
+import cookie from '@/utils/cookie';
 
 export default {
     components: {
         Footer
     },
 
+    data() {
+        return {
+            loading: false,
+        }
+    },
+
     methods: {
         onBackBtn() {
             this.$router.go(-1);
+        },
+
+        onLogin() {
+            this.loading = true
+            window.Kakao.Auth.login({
+                success: this.getKakaoMyInfo
+            })
+        },
+
+        getKakaoMyInfo(authObj) {
+            window.Kakao.API.request({
+                url: '/v2/user/me',
+                success: res => {
+                    const kakao_account = res.kakao_account;
+                    const userInfo = {
+                        kakaoId: res.id,
+                        accessToken: authObj.access_token,
+                        refreshToken: authObj.refresh_token,
+                        expiresIn: authObj.expires_in,
+                        nickname: kakao_account.profile.nickname,
+                        email: kakao_account.email,
+                        phoneNumber: kakao_account.phone_number,
+                        thumbnailImageUrl: kakao_account.profile.thumbnail_image_url
+
+                    }
+
+                    apiLogin.sighUpLogin(userInfo)
+                        .then(response => {
+                            // response.resultCode === 0 인 경우..
+                            if (response.data.resultCode === 0) {
+                                var enmemberidx = response.data.Enmemberidx
+                                cookie.setCookie('Enmemberidx', enmemberidx, 1)
+                                this.$router.go(-1);
+                            } else {
+                                console.log("response.data.resultCode === 0 이 아닌 경우에 대한 예외 처리")
+                            }
+                        })
+                        .catch(e => {
+                            // 예외사항 체크
+                            console.log(e)
+                        });
+                },
+                fail: error => {
+                    console.log(error);
+                }
+            })
         },
     }
 }
@@ -62,5 +120,28 @@ export default {
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    margin-bottom: 26px;
+}
+
+.login-loading-btn {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    margin-bottom: 26px;
+
+    max-width: 300px;
+    height: 45px;
+
+    background: #FEE500;
+    border-radius: 8px;
+
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1px;
+
+    color: #000000;
 }
 </style>
